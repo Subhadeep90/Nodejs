dc37edef4a95167b188f871fb55a70dc23dc93b4
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-03-11 19:36:23 +0530

    nodesecondfile

---
+++ nodesecondfile.js
@@ -0,0 +1,10 @@
+const product=(a,b)=>a*b;
+console.log(product(5,4));
+const student={
+name:"Sam",
+Age:20,
+getdetails(){
+    console.log("Name is"+this.name+" "+"Age is"+this.Age)
+}
+};
+student.getdetails();
