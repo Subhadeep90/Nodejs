01d2fa6b0a121f0e1dd7dfab4f528363e6c1c152
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-03-17 10:55:48 +0530

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const http=require('http');
-const server=http.createServer((req,resolve)=>{
-    console.log(req.headers,req.method,req.url);
-    resolve.setHeader('Content-type','text/html');
-    resolve.write('<html>');
-    resolve.write('<head><title>My first Page</title></head>');
-    resolve.write('<body><h1>My first node js</h1></body>');
-    resolve.write('</html>');
-    resolve.end();
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+    if(url==='/home')
+    {
+        res.write("Welcome to Home");
+        return res.end();
+    }
+    if(url==='/about')
+    {
+        res.write("Welcome to About Us page");
+        return res.end();
+    }
+    if(url==='/about')
+    {
+        res.write("Welcome to About Us page");
+        return res.end();
+    }
+    
+    if(url==='/node')
+    {
+    
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page</title></head>');
+    res.write('<body><h1>Welcome to my Node js project</h1></body>');
+    res.write('</html>');
+    return res.end();
+    }
+    
 })
 server.listen(4000);
