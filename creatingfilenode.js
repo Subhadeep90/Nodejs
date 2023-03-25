460179b3258839c7b7fea74e2b45d4472e47a184
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-03-25 20:00:45 +0530

    modifiedapp

--- creatingfilenode.js
+++ creatingfilenode.js
@@ -1,12 +1,23 @@
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
-  console.log("first middleware");
-  next();
-  
-  
-});
-app.use((req,res,next)=>{
-    console.log("Second middleware");
+const bodyparser=require('body-parser');
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST">Name<input type="text"name ="text">Size<input type="number" name="number"><button>submit</button></form>');
+})
+app.post('/product',(req,res,next)=>{
+console.log(req.body);
+
+res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+
+    res.send("Hello from Express js");
+
+    
+    
   });
-app.listen(3000);
+
+
+    app.listen(3000);
