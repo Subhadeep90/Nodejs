7375b6315c79c986e43fc8366ae3608b753c5c2d
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-04-07 21:41:30 +0530

    usingcontrollers

--- creatingfilenode.js
+++ creatingfilenode.js
@@ -4,6 +4,7 @@ const path=require('path');
 const adminroutes=require('./routes/admin');
 const shoproutes=require('./routes/shop');
 const Contactus=require('./routes/contactus');
+const errorpagetodisplay=require('./Controllers/product');
 
 const bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended:false}));
@@ -12,8 +13,6 @@ app.use(Contactus);
 app.use('/admin',adminroutes);
 app.use(shoproutes);
 
-app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-});
+app.use(errorpagetodisplay.errorpage);
 
     app.listen(3000); 
