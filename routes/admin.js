e8effa7fa114c417df303cdab2b530a136abac83
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-04-16 11:48:31 +0530

    Cartmodel

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const express=require('express');
 const router=express.Router();
 const path=require('path');
 const getproductPagetodisplay=require('../Controllers/product');
-const rootDir=require('../util/path')
+const rootDir=require('../util/path');
 
 router.get('/add-product',getproductPagetodisplay.getProductPage);
 router.post('/add-product',getproductPagetodisplay.getProduct);
-router.get('/add-product/:productId',getproductPagetodisplay.getProductid);
+router.get('add-product/cart',getproductPagetodisplay.cart);
+
+
 
 
 module.exports = router;
