7e8d007125262fe2b20d0fef474e0c17cc4bcbeb
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-04-21 20:28:40 +0530

    commit

--- creatingfilenode.js
+++ creatingfilenode.js
@@ -5,7 +5,7 @@ const adminroutes=require('./routes/admin');
 const shoproutes=require('./routes/shop');
 const Contactus=require('./routes/contactus');
 const errorpagetodisplay=require('./Controllers/product');
-
+// const db=require('./util/database');
 const bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
@@ -14,5 +14,11 @@ app.use('/admin',adminroutes);
 app.use(shoproutes);
 
 app.use(errorpagetodisplay.errorpage);
-
+// db.execute('SELECT * from products')
+// .then(result=>{
+//     console.log(result);
+// })
+// .catch(error=>{
+//     console.log(error)
+// });
     app.listen(3000); 
