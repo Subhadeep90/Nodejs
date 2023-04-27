9c93d62233f99a9383bff4c26d00d68cb5d98ad5
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-04-27 20:57:49 +0530

    Retrieving  data

--- Controllers/product.js
+++ Controllers/product.js
@@ -1,28 +1,42 @@
 
+//const bodyparser=require('body-parser');
 const path=require('path');
 const rootDir=require('../util/path')
 const Product=require('../Models/product');
-const Cart=require('../Models/Cart');
-const fs = require('fs');
-exports.cart=(req,res,next)=>{
-  res.send('<h1>This is the Cart</h1>')
-  
-}
+const { resolveObjectURL } = require('buffer');
 
+//const { appendFile } = require('fs');
+//const Cart=require('../Models/Cart');
+// exports.cart=(req,res,next)=>{
+//   res.send('<h1>This is the Cart</h1>')
+  
+// }
 exports.getProductPage=(req,res,next)=>{
+  
     res.sendFile(path.join(rootDir,'views','add-product.html'))
   };
   exports.getProduct=(req,res,next)=>{
-    const product=new Product(req.body.title);
-    product.save();
-    res.redirect('/');
+     let title=req.body.title;
+    let price=req.body.price;
+    let description=req.body.description;
+    let imageurl=req.body.imageurl;
+
+    const product=new Product(null,title,price,description,imageurl)
+    product.save()
+    .then(()=>{
+      res.redirect('/')})
+    .catch((error)=>{
+     console.log(error);
+    })
     };
     exports.getProductid=(req,res,next)=>{
-      const prodID=req.body.productId;
-      Product.findByid(prodID,(product)=>{
-        Cart.addProduct(prodID,product.price);
-      })
-      res.redirect('/cart');
+      const prodid=req.params.productid;
+     Product.findByid(prodid).then(([product])=>{
+       console.log(product)
+     })
+    .catch((err)=>{
+      console.log(err)
+    })
     }
     
     
@@ -34,12 +48,24 @@ exports.getProductPage=(req,res,next)=>{
         res.send('<h1>Form successfuly filled</h1>')
     }
     exports.contactusPagetoget=(req,res,next)=>{
-        res.sendFile(path.join(rootDir,'views','contactus.html'));
-    }
-    exports.shop=(req,res,next)=>{
-        
-      res.sendFile(path.join(rootDir,'views','shop.html'));
- 
-             
+      Product.fetchALL()
+        .then(([rows,fieldData])=>{
+             res.send(rows);
 
+               
+          //s.sendFile((path.join(rootDir,'views','contactus.html')))
+        
+        }) .catch((error)=>{
+        console.log(error);
+        })  
+      }
+      // exports.contactusPagetoget=(req,res,next)=>{
+        
+      //       res.sendFile((path.join(rootDir,'views','contactus.html')))
+      //     } 
+        
+      
+    exports.shop=(req,res,next)=>{
+      res.sendFile(path.join(rootDir,'views','shop.html'));           
+    
       }
