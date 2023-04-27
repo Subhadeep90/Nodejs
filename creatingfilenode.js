6910a0da4f874fd20a3b2523c8e5b48dbd39892a
Subhadeep90 <subhadeepmitra1990@gmail.com>
2023-04-27 20:56:33 +0530

    Retreiving data

--- creatingfilenode.js
+++ creatingfilenode.js
@@ -1,24 +1,24 @@
+const db=require('./util/database');
 const express=require('express');
 const app=express();
 const path=require('path');
 const adminroutes=require('./routes/admin');
-const shoproutes=require('./routes/shop');
+const shoproutes=require('./routes/shop');   
+// db.execute('SELECT * FROM products')
+// .then((result)=>{
+//     console.log(result);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
 const Contactus=require('./routes/contactus');
 const errorpagetodisplay=require('./Controllers/product');
-// const db=require('./util/database');
 const bodyparser=require('body-parser');
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(Contactus);
 app.use('/admin',adminroutes);
+//app.use(adminroutes)
 app.use(shoproutes);
-
 app.use(errorpagetodisplay.errorpage);
-// db.execute('SELECT * from products')
-// .then(result=>{
-//     console.log(result);
-// })
-// .catch(error=>{
-//     console.log(error)
-// });
- app.listen(3000); 
+ app.listen(5000); 
